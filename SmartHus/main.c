0a4f2a444ad91b9ee7ad280fe0f3be6142d09b66
Allan Holm Iversen <alla1862@elevcampus.dk>
2020-12-07 10:34:31 +0100

    init

---
+++ SmartHus/main.c
@@ -0,0 +1,18 @@
+/*
+ * SmartHus.c
+ *
+ * Created: 07-12-2020 08:18:22
+ * Author : alla1862
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
