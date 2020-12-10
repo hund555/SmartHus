2dfef51b5bb080f881d643e01260a68b4c3a2784
Allan Holm Iversen <alla1862@elevcampus.dk>
2020-12-10 10:37:27 +0100

    Kommentar og billede af flowchart

--- SmartHus/MotionSensor/MostionSensor.c
+++ SmartHus/MotionSensor/MostionSensor.c
@@ -9,8 +9,8 @@
 void MotionSensor_Init()
 {
 	//Sensor interrupt
-	PCICR |= (1<<PCIE0);
-	PCMSK0 |= (1<<PCINT6);
+	PCICR |= (1<<PCIE0);	//PCIE0 use ISR PCINT0_VECT
+	PCMSK0 |= (1<<PCINT6);	//PCINT0-7 are the ones that can use PCINT0_VECT, you can find them on PB0, PB1, PB2, PB3, PB4, PB5, PB6 and PB7.
 }
 
 void Detected_Motion()
