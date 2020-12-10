4e07a7548ea8288469f082261b94a49304344a45
Allan Holm Iversen <alla1862@elevcampus.dk>
2020-12-10 13:32:26 +0100

    Tilføjet kommentar

--- SmartHus/ServoMoter/ServoMoter.c
+++ SmartHus/ServoMoter/ServoMoter.c
@@ -12,8 +12,8 @@ void Servo_Timer_Init()
 	// Compare Output Mode: Fast PWM Mode: Clear OC1A on Compare Match, set OC1A at BOTTOM, non-inverting mode (Table 17-5)
 	TCCR1A |= (1<<COM1A1);
 	
-	//Waveform Generation Mode: Mode 8 Fast PWM: WGMn3 = 1 (Table 17-2)
-	TCCR1B |= (1 << WGM13);
+	//Waveform Generation Mode: Mode 8 Fast PWM, Phase and Frequency Correct: WGMn3 = 1 (Table 17-2)
+	TCCR1B |= (1 << WGM13);		//non-inverted
 	
 	// Clock Select Bit: clk/8 prescaling: CS = 011 : = 1, CS11 = 1 (Table 17-6), frekv. = 50Hz
 	TCCR1B |= (1<<CS11);
